cfaa65ea22658b6e8075429c31b9b7e006c75977
Fares Elsheikh <fares23013093@gmail.com>
2026-07-20 02:35:16 +0300

    Add grade-js

---
+++ grade-js/js/script.js
@@ -0,0 +1,23 @@
+
+let name = "Fares";
+let age = 21;
+let isStudent = true;
+let emptyValue = null;
+let value;
+
+let grade =90;
+if (grade >= 90) {
+    console.log("Excellent");
+}
+else if (grade >= 80) {
+    console.log("Good");
+}
+else if (grade >= 70) {
+    console.log("Average");
+}
+else if (grade >= 60) {
+    console.log("Pass");
+}
+else {
+    console.log("Fail");
+}
